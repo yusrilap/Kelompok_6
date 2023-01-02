f47ecb5974eaf59f49b2f6b534229b8e971432aa
HildaPradita75 <hildapradita75@gmail.com>
2023-01-02 13:57:59 +0700

    Update Position Controller Function Store

--- app/Http/Controllers/Master/PositionController.php
+++ app/Http/Controllers/Master/PositionController.php
@@ -23,6 +23,18 @@ class PositionController extends Controller
         return view('master.position.create', ['title'=>'Tambah Position']);
     }
 
+    public function store(Request $request)
+    {   
+        $request->merge([
+            'salary' => preg_replace('/\D/', '', $request->salary),
+        ]);
+
+        $request->validate([
+            'name'=>'required',
+            'salary'=>'required|max:50|min:3',
+            'status'=>'required'
+        ]);
+
     // TAMBAH DATA
 
     public function destroy()
--- database/seeds/TablePosition.php
+++ database/seeds/TablePosition.php
@@ -7,17 +7,7 @@ class TablePosition extends Seeder
 {
     public function run()
     {
-        $position = [
-            ['name'=>'Ketua Prodi', 'status'=>'Staff', 'salary'=>3000000],
-            ['name'=>'Sekretaris Prodi', 'status'=>'Staff', 'salary'=>5000000],
-            ['name'=>'Koordinator TA', 'status'=>'Staff', 'salary'=>3500000],
-            ['name'=>'Koordinator KP', 'status'=>'Staff', 'salary'=>3500000],
-            ['name'=>'Koordinator LAB', 'status'=>'Staff', 'salary'=>3500000],
-            ['name'=>'Dosen Tetap', 'status'=>'Dosen', 'salary'=>3500000],
-            ['name'=>'Dosen Tidak Tetap', 'status'=>'Dosen', 'salary'=>3500000],
-            ['name'=>'Staff Prodi', 'status'=>'Staff', 'salary'=>3500000],
-
-        ];
+       
         foreach($position as $row)
         {
             Position::create($row);
