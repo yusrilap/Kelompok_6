de8560e1ce2991474f925bf39835c993be3c1886
yusrilap <yusrilazharpermana@gmail.com>
2023-01-04 12:05:18 +0700

    ScheduleController Notif Success dan Delete Data Done

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -66,4 +66,64 @@ class ScheduleController extends Controller
         $data['roles'] = Roles::all();
         return view('master.staff.edit', $data);
     }
+
+    public function update(Request $request, Staff $staff)
+    {
+        $request->validate([
+            'name'=>'required|max:100',
+            'birth'=>'required|date',
+            'startdate'=>'required|date',
+            'phone'=>'required|max:13',
+            'position_id'=>'required',
+            'departement_id'=>'required',
+            'addres'=>'required',
+        ]);
+
+        if ($request->has('makeUserAccount')) {
+            $msg = [
+                'username.min' => 'Username harus terdiri dari minimal 6 karakter.',
+                'username.unique' => 'Username sudah digunakan.'
+            ];
+            $request->validate([
+                'username' => 'required|string|min:6|max:255|unique:users',
+                'role_id' => 'required|integer',
+            ], $msg);
+
+            $user = Users::create([
+                'name' => $request->name,
+                'username' => $request->username,
+                'password' => bcrypt($request->username),
+                'role_id' => $request->role_id
+            ]);
+            $request->request->add(['users_id' => $user->id]);
+        }
+
+        $staff->update($request->all());
+
+        $message = [
+            'alert-type'=>'success',
+            'message'=> 'Data staff updated successfully'
+        ];
+        return redirect()->route('master.staff.index')->with($message);
+    }
+
+    public function destroy(Request $request)
+    {
+        $id = $request->id;
+        if($id)
+        {
+            $staff = Staff::find($id);
+            if($staff)
+            {
+                $staff->delete();
+            }
+            $count = Staff::count();
+            $message = [
+                'alert-type' => 'success',
+                'count' => $count,
+                'message' => 'Data staff deleted successfully.'
+            ];
+            return response()->json($message);
+        }
+    }
 }
