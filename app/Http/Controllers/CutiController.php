4da9117cce75a839b2854d5cf0080defd84ed59a
HildaPradita75 <hildapradita75@gmail.com>
2023-01-10 14:35:08 +0700

    Update Function Store Cuti

--- app/Http/Controllers/CutiController.php
+++ app/Http/Controllers/CutiController.php
@@ -33,4 +33,53 @@ class CutiController extends Controller
         $data['staff'] = Staff::all();
         return view('cuti.create', $data);
     }
+
+    public function store(Request $request)
+    {   
+        $msg = [
+            'staff_id.unique' => 'Staff ini sudah cuti, jika ingin melakukan cuti hapus data cuti lama.',
+        ];
+        $request->validate([
+            'staff_id'=>'required|unique:tb_cuti',
+            'estimasi'=>'required',
+            'keterangan'=>'required',
+        ], $msg);
+
+        $estimasi = explode(" ", $request->estimasi);
+        $started_at = date('Y-m-d', strtotime($estimasi[0]));
+        $finished_at = date('Y-m-d', strtotime($estimasi[2]));
+
+        $startTimeStamp = strtotime($started_at);
+        $endTimeStamp = strtotime($finished_at);
+        $timeDiff = abs($endTimeStamp - $startTimeStamp);
+        $numberDays = $timeDiff/86400;  // 86400 hitungan per hari
+        // konversi ke bilangan
+        $numberDays = intval($numberDays);
+
+        $request->request->add([
+            'tgl_mulai' => $started_at,
+            'tgl_selesai' => $finished_at,
+            'jumlah_cuti' => $numberDays
+        ]);
+
+        if($numberDays > 30)
+        {
+            $message = [
+                'alert-type'=>'error',
+                'message'=> 'Permohonan cuti ('.$numberDays.' hari) melebihi durasi yang dizinkan. Durasi cuti maksimal 30 hari'
+            ];  
+        }
+        else
+        {
+            Cuti::create($request->all());
+
+            $message = [
+                'alert-type'=>'success',
+                'message'=> 'Permohonan cuti created successfully'
+            ];  
+        }
+
+        
+        return redirect()->route('cuti.index')->with($message);
+    }
 }
